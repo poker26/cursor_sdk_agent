acc0f6d82bea0a15ed278b299211668696917910
poker26 <poker26@gmail.com>
2026-05-25 18:14:50 +0300

    Android: compileSdk и targetSdk 35 для androidx.core 1.15

--- android-voice-client/app/build.gradle.kts
+++ android-voice-client/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.begemot.cursorvoice"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.begemot.cursorvoice"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
     }
